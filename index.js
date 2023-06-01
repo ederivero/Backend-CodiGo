bea1bbc60cbc42a281c805856401e8b22d307e4a
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2023-06-01 18:48:08 -0500

    avance

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 import mongoose from "mongoose";
 import express from "express";
 
+const animalSchema = new mongoose.Schema({
+  nombre: {
+    type: mongoose.Schema.Types.String,
+    required: true,
+  },
+  fechaNacimiento: {
+    type: mongoose.Schema.Types.Date,
+    alias: "fecha_nacimiento",
+  },
+  sexo: {
+    type: mongoose.Schema.Types.String,
+    enum: ["MACHO", "HEMBRA"],
+  },
+});
+
+const AnimalModel = mongoose.model("animales", animalSchema);
+
 const conectarMongo = async () => {
   await mongoose.connect("mongodb://127.0.0.1:27017/almacen");
   console.log("Base de datos conectada exitosamente");
